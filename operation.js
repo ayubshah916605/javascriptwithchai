3d685ed86900406df04dbfd38bb9e2d08202654e
AYUB SHAH <ayubshah810@gmail.com>
2023-12-13 23:20:00 +0500

    comparison

---
+++ operation.js
@@ -0,0 +1,13 @@
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//*************comparison**************
+
+
+
+//console.log( 4 != 3)
+//console.log("3" !== 3)
+//console.log(null === 0)
+
+
