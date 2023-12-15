4d132e3246f7728a275d096e0e17c0d244bf5813
AYUB SHAH <ayubshah810@gmail.com>
2023-12-15 22:24:00 +0500

    object file added

---
+++ object.js
@@ -0,0 +1,34 @@
+let students = {
+    fullName: "Ayub",
+    lastName: "Shah",
+    age : 26,
+    isPassed : true,
+}
+// accessing keys and changing the value
+// console.log(students)
+       // if i want to access specific key then there are two ways 
+                            // * .fullNeme i can access
+                            // * ["fullName"]
+//console.log(students.age)
+//console.log(students["isPassed"])
+
+// if i want to reassign the any key value then
+
+// students ["age"] = 27;
+// console.log(students);
+students ["fullName"] = "Alam shah";
+students ["age"] = students ["age"] + 1;
+//console.log(students.fullName);
+//console.log(students);
+const Employee = {
+    name: "Shaji",
+    phonNum: 345676567,
+    isLogedIn: false,
+}
+// console.log(Employee.phonNum)
+// delete Employee.phonNum;
+// console.log(Employee)
+const Profile ={
+    useName: "Ayub shah",
+    followers: 
+}
