a273cd0ba245fa50824de4a36196f58e0175b2e1
AYUB SHAH <ayubshah810@gmail.com>
2023-12-17 01:48:14 +0500

    added string method

--- operation.js
+++ operation.js
@@ -10,4 +10,11 @@ let negValue = -value
 //console.log("3" !== 3)
 //console.log(null === 0)
 
+let str1 = "hello"
+let str2 = " Ayub shah"
+let str3 = str1 + str2
+
+//console.log(str3);
+//console.log((3 + 4) *3 % 2);
+//console.log(+true);
 
